eb76aec41df77f9650e9cc4a7ae4605bc95806a0
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2021-03-18 18:14:48 +0100

    cherry pick ticket change from ticket refactor branch

--- packages/core-connector-interface/src/types/ticket.d.ts
+++ packages/core-connector-interface/src/types/ticket.d.ts
@@ -4,24 +4,6 @@ import Hash from './hash'
 import Signature from './signature'
 import TicketEpoch from './ticketEpoch'
 
-declare interface TicketStatic {
-  readonly SIZE: number
-
-  create(
-    arr?: {
-      bytes: ArrayBuffer
-      offset: number
-    },
-    struct?: {
-      counterparty: AccountId
-      challenge: Hash
-      epoch: TicketEpoch
-      amount: Balance
-      winProb: Hash
-      channelIteration: TicketEpoch
-    }
-  ): Ticket
-}
 declare interface Ticket {
   counterparty: AccountId
   challenge: Hash
@@ -30,16 +12,60 @@ declare interface Ticket {
   winProb: Hash
   channelIteration: TicketEpoch
 
-  // computed properties
   hash: Promise<Hash>
-
   getEmbeddedFunds(): Balance
-
-  toU8a(): Uint8Array
-
-  sign(privKey: Uint8Array): Promise<Signature>
+  sign(privKey: Uint8Array): Promise<SignedTicket>
+  serialize(): Uint8Array
 }
-
+declare interface TicketStatic {
+  SIZE(): number
+  deserialize(Uint8Array): Ticket
+}
 declare var Ticket: TicketStatic
 
-export default Ticket
+declare interface SignedTicket {
+  ticket: Ticket
+  signature: Signature
+  getSigner(): Promise<Uint8Array>
+  verifySignature(pubKey: Uint8Array): Promise<boolean>
+  serialize(): Uint8Array
+  toUnacknowledged(secretA: Hash): UnacknowledgedTicket
+}
+declare interface SignedTicketStatic {
+  readonly SIZE: number
+  deserialize(Uint8Array): SignedTicket
+}
+declare var SignedTicket: SignedTicketStatic
+
+declare interface UnacknowledgedTicket {
+  signedTicket: SignedTicket
+  secretA: Hash
+  serialize(): Uint8Array
+}
+declare interface UnacknowledgedTicketStatic {
+  SIZE(): number
+  deserialize(Uint8Array): UnacknowledgedTicket
+}
+declare var UnacknowledgedTicket: UnacknowledgedTicketStatic
+
+/*
+ * An acknowledged ticket encapsulates the knowledge we have about a ticket
+ * that has been successfully acknowledged by a counterparty, and is waiting
+ * for us to redeem it.
+ *
+ * We don't need to store unsuccessful tickets, or tickets that have been
+ * redeemed.
+ */
+declare interface AcknowledgedTicket {
+  getSignedTicket(): SignedTicket
+  getResponse(): Hash
+  getPreImage(): Hash
+  serialize(): Uint8Array
+}
+declare interface AcknowledgedTicketStatic {
+  SIZE(): number
+  deserialize(Uint8Array): AcknowledgedTicket
+}
+declare var AcknowledgedTicket: AcknowledgedTicketStatic
+
+export { Ticket, SignedTicket, UnacknowledgedTicket, AcknowledgedTicket }
