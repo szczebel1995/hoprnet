e067b64e85af10fddbf70bbc490348832374c511
Jose Aguinaga <me@jjperezaguinaga.com>
2021-05-06 15:37:25 +0200

    Revert "Restoring past changes done in #1592"

--- scripts/nightly.sh
+++ scripts/nightly.sh
@@ -1,5 +1,8 @@
-#!/bin/bash
-set -e #u
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
 
 # Don't source this file twice
 test -z "${NIGHTLY_SOURCED:-}" && NIGHTLY_SOURCED=1 || exit 0
@@ -18,13 +21,18 @@ RELEASE=$(node -p -e "require('./packages/hoprd/package.json').version")
 IMG="gcr.io/hoprassociation/hoprd:$RELEASE"
 
 source scripts/dependencies.sh
+
 echo "Cleaning up devops before running nightly testnet"
 cleanup
+
 echo "Starting nightly testnet"
-start_testnet nightly 2 $IMG
+start_testnet nightly 2 "$IMG"
+
 echo "Testnet up and running. Leaving it for 20 mins"
 sleep 72000 # 20mins
-echo "Testnet has run for 20m, time to kill it."
-gcloud_get_logs nightly-node-2 $IMG > node-2.txt 
-cleanup
 
+echo "Testnet has run for 20m, time to kill it."
+gcloud_get_logs nightly-node-2 "$IMG" > node-2.txt
+
+echo "Cleaning up devops after running nightly testnet"
+cleanup
