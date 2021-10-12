b28c15e7f9e0a5729cf8ffa8040a97224f230747
QYuQianchen <10935300+QYuQianchen@users.noreply.github.com>
2021-10-12 15:20:59 +0200

    cherrypick f253b12b1

--- packages/core-ethereum/src/indexer/index.ts
+++ packages/core-ethereum/src/indexer/index.ts
@@ -20,7 +20,7 @@ import {
 } from '@hoprnet/hopr-utils'
 
 import type { ChainWrapper } from '../ethereum'
-import type { Event, EventNames } from './types'
+import type { Event, EventNames, IndexerEvents, TokenEventNames } from './types'
 import { isConfirmedBlock, snapshotComparator } from './utils'
 
 const log = debug('hopr-core-ethereum:indexer')
@@ -218,7 +218,10 @@ class Indexer extends EventEmitter {
     const nativeTxs = await this.chain.getNativeTokenTransactionInBlock(blockNumber - this.maxConfirmations, true)
     // update transaction manager
     if (nativeTxs.length > 0) {
-      nativeTxs.forEach((nativeTx) => this.chain.updateConfirmedTransaction(nativeTx))
+      this.indexEvent('withdraw-native', nativeTxs)
+      nativeTxs.forEach((nativeTx) => {
+        this.chain.updateConfirmedTransaction(nativeTx)
+      })
     }
 
     // check unconfirmed events and process them if found
@@ -247,14 +250,18 @@ class Indexer extends EventEmitter {
         }
       }
 
-      const eventName = event.event as EventNames
+      const eventName = event.event as EventNames | TokenEventNames
       // update transaction manager
       this.chain.updateConfirmedTransaction(event.transactionHash as string)
       try {
         if (eventName === ANNOUNCEMENT) {
+          this.indexEvent('annouce', [event.transactionHash])
           await this.onAnnouncement(event as Event<'Announcement'>, new BN(blockNumber.toPrecision()))
         } else if (eventName === 'ChannelUpdated') {
           await this.onChannelUpdated(event as Event<'ChannelUpdated'>)
+        } else if (eventName === 'Transfer') {
+          // handle HOPR token transfer
+          this.indexEvent('withdraw-hopr', [event.transactionHash])
         } else {
           log(`ignoring event '${String(eventName)}'`)
         }
@@ -323,6 +330,10 @@ class Indexer extends EventEmitter {
         }
       }
     }
+  }
+  
+  private indexEvent(indexerEvent: IndexerEvents, txHash: string[]) {
+    this.emit(indexerEvent, txHash)
   }
 
   public async getAccount(address: Address) {
