8555593baf7fc37679299087edc07847761121ad
HOPR CI robot <noreply@hoprnet.org>
2021-12-01 15:13:30 +0000

    chore(build): apply linter changes

--- packages/connect/src/index.spec.ts
+++ packages/connect/src/index.spec.ts
@@ -1,8 +1,8 @@
 import LibP2P from 'libp2p'
 import KadDHT from 'libp2p-kad-dht'
 import { NOISE } from '@chainsafe/libp2p-noise'
-import { debug, privKeyToPeerId, wait } from "@hoprnet/hopr-utils"
-import HoprConnect, { HoprConnectOptions } from "."
+import { debug, privKeyToPeerId, wait } from '@hoprnet/hopr-utils'
+import HoprConnect, { HoprConnectOptions } from '.'
 import PeerId from 'peer-id'
 
 const MPLEX = require('libp2p-mplex')
@@ -16,32 +16,33 @@ describe('HoprConnect', function () {
   it('should be able to create a libp2p instance using HoprConnect w/o crashing', async function () {
     this.timeout(3000) // instead of the default 2000ms
     log('starting libp2p w/HoprConnect instance')
-    const libp2pStarter = async () => await LibP2P.create({
-      mockPeerId,
-      addresses: {},
-      // libp2p modules
-      modules: {
-        transport: [HoprConnect as any], // TODO re https://github.com/hoprnet/hopr-connect/issues/78
-        streamMuxer: [MPLEX],
-        connEncryption: [NOISE],
-        dht: KadDHT
-      },
-      config: {
-        // @ts-ignore
-        protocolPrefix: 'hopr',
-        transport: {
-          HoprConnect: {
-            initialNodes: [],
-          } as HoprConnectOptions
+    const libp2pStarter = async () =>
+      await LibP2P.create({
+        mockPeerId,
+        addresses: {},
+        // libp2p modules
+        modules: {
+          transport: [HoprConnect as any], // TODO re https://github.com/hoprnet/hopr-connect/issues/78
+          streamMuxer: [MPLEX],
+          connEncryption: [NOISE],
+          dht: KadDHT
         },
-        dht: {
-          enabled: true
-        },
-        relay: {
-          enabled: false
+        config: {
+          // @ts-ignore
+          protocolPrefix: 'hopr',
+          transport: {
+            HoprConnect: {
+              initialNodes: []
+            } as HoprConnectOptions
+          },
+          dht: {
+            enabled: true
+          },
+          relay: {
+            enabled: false
+          }
         }
-      }
-    })
+      })
     log('libp2p node starter', libp2pStarter)
     // libp2pStarter()
     log('waiting to ensure no immediate crashes are undetected')
